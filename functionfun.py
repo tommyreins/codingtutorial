cf8ede41c27466eb2f692e071146a6132a93fa7f
tommy <tommy.reins@gmail.com>
2019-11-08 19:16:47 -0800

    added a file to showcase functions

---
+++ functionfun.py
@@ -0,0 +1,10 @@
+# function name: "getmoney"
+# behavior: prints the string "cash"
+def getmoney():
+    print("cash")
+
+# run the function 10 times
+i = 0
+while(i < 10):
+    getmoney()
+    i+=1
