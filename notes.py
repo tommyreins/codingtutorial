822dbc45bc3be2352ecfad0ef6d54811edfa9be1
tommy <tommy.reins@gmail.com>
2019-11-08 18:23:01 -0800

    create repo with notes

---
+++ notes.py
@@ -0,0 +1,24 @@
+# What do we know so far?
+
+# print
+# get input
+# if, else
+# while loop
+# integer, float, strings
+# AND vs OR
+
+# WHAT'S NEXT...
+
+# commenting / formatting
+# arrays
+# debugging
+# for each loop
+# nested loops
+# functions
+# importing stuff
+# stack overflow
+
+# Down the road stuff...
+# databases
+# recursion
+# poop! 
